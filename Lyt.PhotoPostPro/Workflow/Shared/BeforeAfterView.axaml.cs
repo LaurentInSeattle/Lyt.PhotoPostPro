f3bfe1bfd7dab181804bf601c4f9b09bc3727c6e
Laurent <ly.testud@outlook.com>
2026-06-20 12:22:20 -0700

    Created a shared viewer for before vs after effects

---
+++ Lyt.PhotoPostPro/Workflow/Shared/BeforeAfterView.axaml.cs
@@ -0,0 +1,9 @@
+namespace Lyt.PhotoPostPro.Workflow.Process.Shared;
+
+public partial class BeforeAfterView : UserControl
+{
+    public BeforeAfterView()
+    {
+        InitializeComponent();
+    }
+}
