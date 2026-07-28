a9b627e8d7323fa00dbcffe69abbaaab30b1ace6
Laurent <ly.testud@outlook.com>
2026-07-28 08:55:34 -0700

    Added spinner for export process

--- Lyt.PhotoPostPro/Workflow/Process/Export/ExportToolboxViewModel.cs
+++ Lyt.PhotoPostPro/Workflow/Process/Export/ExportToolboxViewModel.cs
@@ -2,20 +2,54 @@
 
 public sealed partial class ExportToolboxViewModel : ToolboxViewModel<ExportToolboxView, ExportStep> 
 {
+    public ExportToolboxViewModel()
+    {
+        this.SpinViewModel = new SpinViewModel()
+        {
+            IsVisible = false,
+            IsActive = false,
+        };
+    }
+
     protected override string Title => this.Localize("Workflow.Export.Title");
+
+    [ObservableProperty]
+    public partial SpinViewModel SpinViewModel { get; set; }
+
+    private void SpinWait(bool start = true)
+    {
+        this.SpinViewModel.IsVisible = start;
+        this.SpinViewModel.IsActive = start;
+    }
 
     [RelayCommand]
     public void OnExport()
     {
         // TODO: Collect parameters 
-        this.model.Export(new ExportParameters());
+        ExportParameters exportParameters = new();
+
+        // Always launch a spinner for big or small files 
+        this.SpinWait(start: true);
+        Task.Run(() => 
+        {
+            try
+            {
+                this.model.Export(exportParameters);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            } 
+            finally
+            {
+                // Error or not: stop the spinner 
+                Dispatch.OnUiThread(() => { this.SpinWait(start: false); });
+            }
+        });
     }
 
     [RelayCommand]
-    public void OnNavigate()
-    {
-        this.model.NavigateToExport();
-    }
+    public void OnNavigate() => this.model.NavigateToExport();
 
     [RelayCommand]
     public void OnFinish()
