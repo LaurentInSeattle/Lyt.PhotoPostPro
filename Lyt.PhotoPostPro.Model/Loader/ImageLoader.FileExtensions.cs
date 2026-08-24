4a29698632a6f0b2d9abbab5b5bbba4871e154ca
Laurent <ly.testud@outlook.com>
2026-08-24 15:36:11 -0700

    Image loader: added more known raw image file extensions. Split file.

---
+++ Lyt.PhotoPostPro.Model/Loader/ImageLoader.FileExtensions.cs
@@ -0,0 +1,88 @@
+﻿namespace Lyt.PhotoPostPro.Model.Loader;
+
+public static partial class ImageLoader
+{
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+
+    public static List<string> ExcludedExtensions = [".aae", ".docx", ".xlsx", ".pdf"];
+
+    public static bool HasExcludedExtension(string path)
+        => ExcludedExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
+
+    public static List<string> MovieExtensions = [".mp4", ".mov", ".mkv", ".avi", ".webm"];
+
+    public static bool HasMovieExtension(string path)
+        => MovieExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
+
+    public static List<string> HeifExtensions = [".heic", ".heif", ".hif"];
+
+    public static bool HasHiefExtension(string path)
+        => HeifExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
+
+    // https://en.wikipedia.org/wiki/Raw_image_format
+    // Many raw file formats, including IIQ (Phase One), 3FR (Hasselblad), DCR, K25, KDC (Kodak),
+    // CRW, CR2 (Canon), ERF (Epson), MEF (Mamiya), MOS (Leaf), NEF NRW (Nikon), ORF (Olympus),
+    // PEF (Pentax), RW2 (Panasonic) and ARW, SRF, SR2 (Sony), are based on TIFF, the Tag Image
+    // File Format.[2]
+    //
+    // These files may deviate from the TIFF standard in a number of ways, including the use of a
+    // non-standard file header, the inclusion of additional image tags and the encryption of some
+    // of the tag data. 
+    public static List<string> RawExtensions =
+        [
+            // Manufacturers 
+            ".ari", // Arri Alexa
+            ".bay", // Casio
+            ".braw", // Blackmagic Design
+            ".cri",  // Cintel
+            ".iqq", ".cap", ".eip", // Phase One 
+            ".3fr", // Hasselblad
+            ".mos", // Leaf
+            ".mef", // Mamiya
+            ".pef", ".ptx", // Pentax
+            ".erf", // Epson
+            ".crw" ,".cr2" , ".cr3", // Canon 
+            ".nef" , ".nrw", // Nikon 
+            ".arw" , "srf", "sr2", // Sony 
+            ".raf" , // Fuji 
+            ".rw2" , // Leica / Panasonic 
+            ".orf" , // Olympus 
+            ".dcr", ".k25", ".kdc", // Kodak, 
+            ".fff", // Imacon / Hasselblad
+            ".gpr", // GoPro
+            ".mdc", // Minolta, Agfa
+            ".mrw", // Minolta, Konica Minolta
+            ".pxn", // Logitech
+            ".r3d", // RED Digital Cinema
+            ".rwl", // Leica
+            ".rwz", // Rawzor
+            ".srw", // Samsung
+            ".tco", // intoPIX
+            ".x3f", // Sigma
+            // 
+            ".dng" , // Adobe 
+            ".raw" , // Generic 
+        ];
+
+    public static bool HasRawExtension(string path)
+        => RawExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
+
+    public static List<string> ImageSharpExtensions =
+        [
+            ".tiff", ".cur", ".png", ".pbm", ".qoi", ".tga",
+            ".webp", ".ico", ".gif", ".jpg", ".jpeg", ".jfif" , ".bmp", ".exr",
+        ];
+
+    public static bool HasImageSharpExtension(string path)
+        => ImageSharpExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
+
+    public static List<string> JpgExtensions =
+        [
+            ".jpg", ".jpeg", ".jfif" ,
+        ];
+
+    public static bool HasJpgExtension(string path)
+        => JpgExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
+
+#pragma warning restore CA2211 // Non-constant fields should not be visible
+}
--- Lyt.PhotoPostPro.Model/Loader/ImageLoader.cs
+++ Lyt.PhotoPostPro.Model/Loader/ImageLoader.cs
@@ -3,84 +3,13 @@
 // Dont move to Global Usings : Conflicting with ImageSharp 
 using Openize.Heic.Decoder;
 
-using System.Net.Http.Headers;
-
-public static class ImageLoader
+public static partial class ImageLoader
 {
     public const int ThumbnailQuality = 80;
     public const int HdImageQuality = 90;
     public const int ThumbnailLargestDimension = 420;
     public const int HdWidth = 1920;
     public const int HdHeight = 1080;
-
-#pragma warning disable CA2211 // Non-constant fields should not be visible
-
-    public static List<string> ExcludedExtensions = [".aae", ".docx", ".xlsx", ".pdf"];
-
-    public static bool HasExcludedExtension(string path)
-        => ExcludedExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
-
-    public static List<string> MovieExtensions = [".mp4", ".mov", ".mkv", ".avi", ".webm"];
-
-    public static bool HasMovieExtension(string path)
-        => MovieExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
-
-    public static List<string> HeifExtensions = [".heic", ".heif", ".hif"];
-
-    public static bool HasHiefExtension(string path)
-        => HeifExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
-
-    // https://en.wikipedia.org/wiki/Raw_image_format
-    // Many raw file formats, including IIQ (Phase One), 3FR (Hasselblad), DCR, K25, KDC (Kodak),
-    // CRW, CR2 (Canon), ERF (Epson), MEF (Mamiya), MOS (Leaf), NEF NRW (Nikon), ORF (Olympus),
-    // PEF (Pentax), RW2 (Panasonic) and ARW, SRF, SR2 (Sony), are based on TIFF, the Tag Image
-    // File Format.[2]
-    //
-    // These files may deviate from the TIFF standard in a number of ways, including the use of a
-    // non-standard file header, the inclusion of additional image tags and the encryption of some
-    // of the tag data. 
-    public static List<string> RawExtensions =
-        [
-            // Manufacturers 
-            ".iqq", // Phase One 
-            ".3fr", // Hasselblad
-            ".mos", // Leaf
-            ".mef", // Mamiya
-            ".pef", // Pentax
-            ".erf", // Epson
-            ".crw" ,".cr2" , ".cr3", // Canon 
-            ".nef" , ".nrw", // Nikon 
-            ".arw" , "srf", "sr2", // Sony 
-            ".raf" , // Fuji 
-            ".rw2" , // Leica / Panasonic 
-            ".orf" , // Olympus 
-            ".dcr", ".k25", ".kdc", // Kodak, 
-            // 
-            ".dng" , // Adobe 
-            ".raw" , // Generic 
-        ];
-
-    public static bool HasRawExtension(string path)
-        => RawExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
-
-    public static List<string> ImageSharpExtensions =
-        [
-            ".tiff", ".cur", ".png", ".pbm", ".qoi", ".tga",
-            ".webp", ".ico", ".gif", ".jpg", ".jpeg", ".jfif" , ".bmp", ".exr",
-        ];
-
-    public static bool HasImageSharpExtension(string path)
-        => ImageSharpExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
-
-    public static List<string> JpgExtensions =
-        [
-            ".jpg", ".jpeg", ".jfif" ,
-        ];
-
-    public static bool HasJpgExtension(string path)
-        => JpgExtensions.Contains(System.IO.Path.GetExtension(path).ToLower());
-
-#pragma warning restore CA2211 // Non-constant fields should not be visible
 
     public static string LibRawVersion => RawContext.Version;
 
