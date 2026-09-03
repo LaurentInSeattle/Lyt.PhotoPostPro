0d5514fd2266c81aaa1fb01dea7d9ff2801fe9a3
Laurent <ly.testud@outlook.com>
2026-09-03 12:13:21 -0700

    WIP - Started editor + localization run

---
+++ Lyt.PhotoPostPro/Workflow/Tools/Editor/EditorView.axaml.cs
@@ -0,0 +1,6 @@
+namespace Lyt.PhotoPostPro.Workflow.Tools.Editor;
+
+public partial class EditorView : View
+{
+    protected override void OnDataContextChanged(object? sender, EventArgs e) { }
+}
---
+++ Lyt.PhotoPostPro/Workflow/Tools/Editor/EditorViewModel.cs
@@ -0,0 +1,63 @@
+﻿namespace Lyt.PhotoPostPro.Workflow.Tools.Editor;
+
+public interface IEditableObject
+{
+    string FriendlyName { get; set; }
+
+    UserControl EditingForm { get; set; }
+}
+
+public sealed partial class EditorViewModel : ViewModel<EditorView>
+{
+    private readonly PhotoPostProModel model;
+    private bool isFirstActivation;
+
+    [ObservableProperty]
+    // The collection of editable items in the master list - left side 
+    public partial ObservableCollection<IEditableObject> EditableObjects { get; set; } = [];
+
+    [ObservableProperty]
+    // the currently selected editable item in the master list 
+    public partial IEditableObject? SelectedObject { get; set; }
+
+    [ObservableProperty]
+    // SelectedIndex in the master list 
+    public partial int SelectedObjectIndex { get; set; }
+
+    [ObservableProperty]
+    // Selected object name 
+    public partial string FriendlyName { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial UserControl? EditingForm { get; set; } 
+
+    public EditorViewModel(PhotoPostProModel model)
+    {
+        this.model = model;
+        this.isFirstActivation = true;
+    }
+
+    public override void Activate(object? activationParameters)
+    {
+        base.Activate(activationParameters);
+
+        if (this.isFirstActivation)
+        {
+            // This cannot be done in the constructor
+            this.isFirstActivation = false;
+        }
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        this.EditableObjects.Clear() ;
+    }
+
+    partial void OnSelectedObjectIndexChanged(int value)
+    {
+        var selected = this.SelectedObject;
+
+    }
+
+}
